9a5c0b85c061c3b07736de68918eb1680518feed
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-03-25 20:22:11 +0530

    update: smartContract

--- Web3/crowdFund.sol
+++ Web3/crowdFund.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.26;
 
 contract crowdFunding {
     uint256 private counter;
@@ -32,26 +32,27 @@ contract crowdFunding {
         emit CampaignCreated(msg.sender, newCampaign.ID, Amount, Description, img);
     }
 
-    function donateCampaign(address payable campaignOwner, uint256 donationAmount) external payable {
+    function donateCampaign(address payable campaignOwner) external payable {
         require(campaignOwner != address(0), "Invalid campaign owner address");
         require(crowdFundData[campaignOwner].ID != 0, "Campaign owner does not exist");
-        require(donationAmount > 0, "Invalid donation amount");
+        require(msg.value > 0, "Invalid donation amount");
 
-        (bool success, ) = campaignOwner.call{value: donationAmount, gas: 500000}("");
+        (bool success, ) = campaignOwner.call{value: msg.value, gas: 500000}("");
         require(success, "Failed to send Ethers");
 
-        emit DonationMade(msg.sender, campaignOwner, donationAmount);
-    }
-
-    function sendToContract(uint256 amount, address payable contractAddress) public payable {
-        (bool success, ) = contractAddress.call{value: amount, gas: 1000000}("money sent to the contract");
-        require(success, "Failed to send Ethers");
-
-        emit EtherSentToContract(msg.sender, contractAddress, amount);
+        emit DonationMade(msg.sender, campaignOwner, msg.value);
     }
 
     // Function to send Ether to the contract
     function fundContract() public payable {
         // This function can be used to send Ether to the contract
+    }
+
+    function contractBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function addressBalance(address user) public view returns(uint256){
+        return user.balance;
     }
 }
